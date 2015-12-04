421e3937316941dd59af7cb65b0865e2449ef71a
Andreas Grohmann <a.grohmann@epages.com>
2015-12-04 18:27:22 +0100

    add products page view start

---
+++ AndroidStudio/ePagesShopping/app/src/main/java/com/epages/epagesshopping/ProductsPagerViewActivity.java
@@ -0,0 +1,68 @@
+package com.epages.epagesshopping;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//activity class to show ePages objects in fragments
+public class ProductsPagerViewActivity extends ActionBarActivity {
+    MyPageAdapter pageAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        //call super constructor
+        super.onCreate(savedInstanceState);
+
+        //select layout
+        setContentView(R.layout.products_pager_view_activity);
+
+        List<Fragment> fragments = getFragments();
+
+        pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
+
+        ViewPager pager = (ViewPager)findViewById(R.id.viewpager);
+        pager.setAdapter(pageAdapter);
+
+    }
+    //get fragments for view pager object
+    private List<Fragment> getFragments(){
+        List<Fragment> fList = new ArrayList<Fragment>();
+
+        //TODO: get products with rest count results and create fragments with JSON object content
+        JSONObject testJSONObject = new JSONObject("{\"name\":\"test product 123\",\"id\":\"1234567\"}");
+
+        //fList.add(FirstFragment.newInstance(0, "Page # 1 with seperate fragment class"));
+        fList.add(ProductsPagerViewFragment.newInstance(testJSONObject));
+        fList.add(ProductsPagerViewFragment.newInstance(testJSONObject));
+        fList.add(ProductsPagerViewFragment.newInstance(testJSONObject));
+
+        return fList;
+    }
+
+    private class MyPageAdapter extends FragmentPagerAdapter {
+        private List<Fragment> fragments;
+
+        public MyPageAdapter(FragmentManager fm, List<Fragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return this.fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return this.fragments.size();
+        }
+    }
+}
---
+++ AndroidStudio/ePagesShopping/app/src/main/java/com/epages/epagesshopping/ProductsPagerViewFragment.java
@@ -0,0 +1,35 @@
+package com.epages.epagesshopping;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+public class ProductsPagerViewFragment extends Fragment {
+
+    public static final String PRODUCT_NAME = "PRODUCT_NAME";
+
+    public static final ProductsPagerViewFragment newInstance(JSONObject ePagesJSONProduct)
+    {
+        ProductsPagerViewFragment f = new ProductsPagerViewFragment();
+        Bundle bdl = new Bundle(1);
+        bdl.putString(PRODUCT_NAME, ePagesJSONProduct.getString("name"));
+        f.setArguments(bdl);
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        String message = getArguments().getString(PRODUCT_NAME);
+        View v = inflater.inflate(R.layout.products_pager_view_fragment, container, false);
+        TextView messageTextView = (TextView)v.findViewById(R.id.productsPagerViewProductName);
+        messageTextView.setText(message);
+
+        return v;
+    }
+}
